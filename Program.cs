832bf3335cfe2b7ec28fd62763f2d2ae792c3fca
John Sjödin <sjodin.j@gmail.com>
2026-09-15 13:21:04 +0200

    Lade till CORS

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()));
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
