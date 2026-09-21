18d7c3f82dc0e3f51a0cecef97f3d57594fa9a7c
John Sjödin <sjodin.j@gmail.com>
2026-09-21 20:29:45 +0200

    Inputvalidering

--- Models/Faction.cs
+++ Models/Faction.cs
@@ -1,12 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FactionFactory.Api.Models;
 
 public record Faction
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Motto { get; set; }
-    public string? Description { get; set; }
-    public string? Type { get; set; }
-    public string? Organisation { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MaxLength(200)]
+    public string Motto { get; set; }
+
+    [MaxLength(2000)]
+    public string Description { get; set; }
+
+    [MaxLength(50)]
+    public string Type { get; set; }
+
+    [MaxLength(50)]
+    public string Organisation { get; set; }
+
     public string? EmblemFileName { get; set; }
 }
