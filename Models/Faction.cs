ee13aa5aaf6b1f2bd11d73641975fae3ab896909
John Sjödin <sjodin.j@gmail.com>
2026-09-21 20:42:27 +0200

    Valideringsuppdatering

--- Models/Faction.cs
+++ Models/Faction.cs
@@ -8,19 +8,19 @@ public record Faction
 
     [Required]
     [MaxLength(100)]
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
 
     [MaxLength(200)]
-    public string Motto { get; set; }
+    public string Motto { get; set; } = "";
 
     [MaxLength(2000)]
-    public string Description { get; set; }
+    public string Description { get; set; } = "";
 
     [MaxLength(50)]
-    public string Type { get; set; }
+    public string Type { get; set; } = "";
 
     [MaxLength(50)]
-    public string Organisation { get; set; }
+    public string Organisation { get; set; } = "";
 
     public string? EmblemFileName { get; set; }
 }
